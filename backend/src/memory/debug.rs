3731c5eabbb8657af8bb936e6ae2299c1d09570f
Lucien Le Roux <rshn.kabu@gmail.com>
2022-04-15 17:08:13 +0200

    rename MemoryDebug.disassembly

--- backend/src/memory/debug.rs
+++ backend/src/memory/debug.rs
@@ -8,14 +8,14 @@ use crate::{
 #[derive(Serialize)]
 pub struct MemoryDebug {
     ram: Vec<u8>,
-    program: Vec<Disassembly>,
+    disassembly: Vec<Disassembly>,
 }
 
 impl Probe<MemoryDebug> for Memory {
     fn get_debug (&self) -> MemoryDebug {
         MemoryDebug {
             ram: self.ram.to_vec(),
-            program: (0 .. MEMORY_SIZE).step_by(2).map(|n| Disassembly::new(self.fetch(n), n as u16)).collect()
+            disassembly: (0 .. MEMORY_SIZE).step_by(2).map(|n| Disassembly::new(self.fetch(n), n as u16)).collect()
         }
     }
 }
